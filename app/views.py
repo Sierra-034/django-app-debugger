09c7c229d17d7ed4658d1e4a10b3d897a2149ac5
Samuel Gómez Balderas <samuel.gomez.balderas@gmail.com>
2024-03-09 21:06:07 -0600

    first simple view

--- app/views.py
+++ app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create you own views
+# Create your views here.
+class MyView(APIView):
+    def get(self, request, format=None):
+        return Response({'message': 'operation success'})
